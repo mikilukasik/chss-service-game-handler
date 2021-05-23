6181c929786146334c1dd4c9a616383e03f24a08
Miklos Lukasik <miklosbacsi@gmail.com>
2021-05-23 04:10:05 +0100

    remove outdated comment

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -43,9 +43,6 @@ export const getNextGameState = async({ game, updateProgress }) => {
 
   if (!result.length) return null;
 
-  // hack, to get the worst move. computer just wanna loose, works hard on it
-  // result.sort((a, b) => a.score - b.score);
-
   result.sort((a, b) => b.score - a.score);
 
   const moveCoords = result[0].moveTree[0];
